e81d98395466cb67e1a9cff67e5672dc8e966b67
Edgar Pérez <101810702+edgarfrancisco2022@users.noreply.github.com>
2022-03-17 11:29:13 -0600

    add singly linked list

---
+++ src/singly_linked_list/SinglyLinkedList.java
@@ -0,0 +1,98 @@
+package singly_linked_list;
+
+public class SinglyLinkedList<E> {
+
+//-------------Nested Node class-------------//
+    private static class Node<E> {
+        private E element;
+        private Node<E> next;
+
+        public Node(E e, Node<E> n) {
+            this.element = e;
+            this.next = n;
+        }
+
+        public E getElement() {
+            return element;
+        }
+
+        public Node<E> getNext() {
+            return next;
+        }
+
+        public void setNext(Node<E> n) {
+            this.next = n;
+        }
+    }
+
+    private Node<E> head = null;
+    private Node<E> tail = null;
+    private int size = 0;
+
+    public SinglyLinkedList() {}
+
+    /* Methods to implement
+    size()
+    isEmpty()
+    first()
+    last()
+    addFirst(e)
+    addLast(e)
+    removeFirst(e)
+     */
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public E first() {
+        if (isEmpty()) {
+            return null;
+        }
+        return head.getElement();
+    }
+
+    public E last() {
+        if (isEmpty()) {
+            return null;
+        }
+        return tail.getElement();
+    }
+
+    public void addFirst(E e) {
+        head = new Node(e, head);
+        if (size == 0) {
+            tail = head;
+        }
+        size++;
+    }
+
+    public void addLast(E e) {
+        Node<E> newest = new Node(e, null);
+        if (size == 0) {
+            head = newest;
+        } else {
+            tail.setNext(newest);
+        }
+        tail = newest;
+        size++;
+    }
+
+    public E removeFirst() {    //returns removed element
+        if (isEmpty()) {
+            return null;
+        }
+        E answer = head.getElement();
+        head = head.getNext();
+        size--;
+        if (size == 0) {
+            tail = null;
+        }
+        return answer;
+    }
+
+}
