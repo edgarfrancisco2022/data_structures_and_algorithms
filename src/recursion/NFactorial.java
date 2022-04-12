e466353fad6142b5f6afadd4284d05696e3aa897
Edgar Pérez <101810702+edgarfrancisco2022@users.noreply.github.com>
2022-04-12 17:52:48 -0500

    add recursion examples - NFactorial and DrawEnglishRuler

---
+++ src/recursion/NFactorial.java
@@ -0,0 +1,19 @@
+package recursion;
+
+public class NFactorial {
+/* --------------------------Find n! function---------------------------------------*/
+    public static int factorial(int n) throws IllegalArgumentException {
+        if (n < 0) {
+            throw new IllegalArgumentException();
+        } else if (n == 0) {
+            return 1;
+        } else {
+            return n * factorial(n - 1);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(factorial(5));
+    }
+}
